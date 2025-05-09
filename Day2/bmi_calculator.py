4dac24e12f3bee395ae8892269e3fc66d135267a
Abdul Hannan <122171685+AbdulHannan018@users.noreply.github.com>
2025-05-09 20:34:24 +0500

    Delete Day2 directory
    Renaming it to Day***

--- Day2/bmi_calculator.py
+++
@@ -1,9 +0,0 @@
-height = 1.65
-weight = 84
-
-# Write your code here.
-# Calculate the bmi using weight and height.
-bmi = weight / (height**2)
-
-print(round(bmi))  # rounding off
-print(round(bmi, 2))  # rounding off upto 2 decimal places
