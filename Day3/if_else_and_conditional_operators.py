fd4b98654ab1e1c1897f99a5137fcfc674d63c87
Abdul Hannan <122171685+AbdulHannan018@users.noreply.github.com>
2025-05-09 20:34:38 +0500

    Delete Day3 directory

--- Day3/if_else_and_conditional_operators.py
+++
@@ -1,8 +0,0 @@
-# if else: if cond. True, do this. else (cond. False), do that.
-height = int(input("Enter your height (cm): "))
-if height >= 120:
-    print("Go, Ride.")
-else:
-    print("You are not tall enough to ride.")
-
-# Comparison Operators: <, >, <=, >=, ==, !=
