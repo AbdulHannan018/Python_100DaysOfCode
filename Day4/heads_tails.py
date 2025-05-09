be90c52602ac99548e4dec2143d04769faaa82b0
Abdul Hannan <122171685+AbdulHannan018@users.noreply.github.com>
2025-05-09 20:34:54 +0500

    Delete Day4 directory
    Renaming it to Day***

--- Day4/heads_tails.py
+++
@@ -1,7 +0,0 @@
-import random
-random_integer = random.randint(1,10)
-print(random_integer)
-if random_integer % 2 == 1:
-    print("Head")
-else:
-    print("Tail")
--- Day4/who_will_pay_the_bill.py
+++
@@ -1,9 +0,0 @@
-import random
-friends = ["Zuzi", "Impatient", "Ibrahim", "Abdul"]
-person_paying_bill = random.choice(friends)
-print(person_paying_bill)
-
-# another solution
-random_index = random.randint(0,3)
-print(friends[random_index])
-
